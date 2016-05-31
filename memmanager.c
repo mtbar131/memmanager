6ae0ac0ca1f5c29423d5e045ca86b6122f443be0
0xdeadbeef <faiz_84123@yahoo.com>
2016-05-30 21:46:20 -0700

    Design change. Global malloc and free fn

--- memmanager.c
+++ memmanager.c
@@ -4,9 +4,16 @@ memmanager * mem_manager_get(int type) {
 	memmanager *m = (memmanager *) malloc(sizeof(memmanager));
 	switch(type) {
 		case KERNEL:
-			m->malloc = malloc;
-			m->free = free;
+			/* Every allocator should have its own init fn like kern_init(m)*/
 			return m;
 	}
 	return NULL;
+}
+
+void * mem_malloc(memmanager *m, size_t size) {
+	return m->malloc(size);
+}
+
+void mem_free(memmananger *m, void *ptr) {
+	m->free(ptr);
 }
